235793901efc2b6461eaa1e4744118c0c00295b1
polint <polint@uw.edu>
2020-05-05 11:59:38 -0700

    brainstorming plotting functionality

---
+++ plotmodule.py
@@ -0,0 +1,9 @@
+# for plotting heatmap of object coordinates
+
+# how this will work:
+#   - have a graphing flag to distinguish graphing bits of code
+#   - in synthofself: append coords of detected objects to arrays
+#       of x and y values
+#   - convert list of coords to heatmap via matplotlib
+#   - use animate function in matplotlib to update heatmap in real time
+#   - add option of saving output / logs
